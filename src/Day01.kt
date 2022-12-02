e0f864490fe4c13022da0ea674c427b3b957b112
3n3a <gh@3n3a.ch>
2022-12-02 14:22:25 +0000

    complete day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,30 @@
 fun Day01() {
+    fun preprocess(input: List<String>): List<Int> {
+        return input 
+          .joinToString("|")
+          .split("||")
+          .map { it.split("|").map { it.toInt() }.sum() }
+    }
+
     fun part1(input: List<String>): Int {
-        return input.size
+        val processedInput = preprocess(input)
+        return processedInput.maxOrNull()?: 0
     }
     
     fun part2(input: List<String>): Int {
-        return input.size
+        val processedInput = preprocess(input)
+        return processedInput
+          .sortedDescending()
+          .slice(IntRange(0, 2))
+          .sum()
     }
 
-    // test if implementation meets criteria from the description, like:
-    val testInput = readInput("Day02_test")
-    check(part1(testInput) == 1)
+    // test if implementation meets criteria from the description
+    val testInput = readInput("Day01_test")
+    check(part1(testInput) == 24000)
+    check(part2(testInput) == 45000)
     
-    val input = readInput("Day02")
+    val input = readInput("Day01")
     println(part1(input))
     println(part2(input))
 }
