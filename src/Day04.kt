7275977e6adb0d6cdb1518d8262d0ee93f8c1017
3n3a <46775561+3n3a@users.noreply.github.com>
2022-12-04 11:36:29 +0100

    add day4 finished

--- src/Day04.kt
+++ src/Day04.kt
@@ -1,24 +1,39 @@
 fun Day04() {
-    fun preprocess(input: List<String>) {
+    fun preprocess(input: List<String>): List<List<Set<Int>>> {
         return input
+            .map { it.split(",") } // to pair of elves
+            .map { it.map { it2 -> it2.split("-") } } // each elf gets pair of numbers
+            .map { it.map { it2 -> IntRange(it2[0].toInt(), it2[1].toInt()).toSet() }} // each elf gets range of numbers
     }
 
     fun part1(input: List<String>): Int {
         val processed = preprocess(input)
-
-        return input.size
+        var fullyContainedSets = 0
+        for ((elfOne, elfTwo) in processed) {
+            if (elfOne.containsAll(elfTwo) ||
+                elfTwo.containsAll(elfOne)) {
+                fullyContainedSets = fullyContainedSets.inc()
+            }
+        }
+        return fullyContainedSets
     }
 
     fun part2(input: List<String>): Int {
         val processed = preprocess(input)
-
-        return input.size
+        var intersectedSets = 0
+        for ((elfOne, elfTwo) in processed) {
+            if (elfOne.intersect(elfTwo).isNotEmpty() ||
+                elfTwo.intersect(elfOne).isNotEmpty()) {
+                intersectedSets = intersectedSets.inc()
+            }
+        }
+        return intersectedSets
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day04_test")
     check(part1(testInput) == 2)
-    check(part2(testInput) == 1)
+    check(part2(testInput) == 4)
 
     val input = readInput("Day04")
     println("1: " + part1(input))
