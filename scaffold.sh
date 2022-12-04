e39edd259d4e43efa2cdfff936eb1deec7948c18
3n3a <46775561+3n3a@users.noreply.github.com>
2022-12-04 11:46:33 +0100

    add scaffold script

---
+++ scaffold.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Scaffold New Day
+#
+
+DAY_NUMBER="05"
+SRC_PATH="src"
+TEMPLATE_PATH="Template.txt"
+
+function create_input_files() {
+    (
+      cd "$SRC_PATH" || exit
+      touch "Day$DAY_NUMBER.txt"
+      touch "Day$DAY_NUMBER\_test.txt"
+      create_code_file
+    )
+}
+
+function create_code_file() {
+    CODE_FILE="Day$DAY_NUMBER.kt"
+    cp "$TEMPLATE_PATH" "$CODE_FILE"
+    sed -i "s/Template/Day$DAY_NUMBER/" "$CODE_FILE"
+}
+
+echo "Creating Files for Day$DAY_NUMBER"
+create_input_files
